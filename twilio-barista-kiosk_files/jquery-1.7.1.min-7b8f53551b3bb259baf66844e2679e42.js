5dbbc0bd306481973d6e0bcdf836f4320014e5ac
mplacona <marcos.placona@gmail.com>
2016-06-06 17:21:34 +0100

    Initial add

---
+++ twilio-barista-kiosk_files/jquery-1.7.1.min-7b8f53551b3bb259baf66844e2679e42.js
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>404 Not Found</title>
+</head><body>
+<h1>Not Found</h1>
+<p>The requested URL /assets/jquery-1.7.1.min-7b8f53551b3bb259baf66844e2679e42.js was not found on this server.</p>
+<hr>
+<address>Apache/2.2.15 (CentOS) Server at rickyrobinett.com Port 80</address>
+</body></html>
